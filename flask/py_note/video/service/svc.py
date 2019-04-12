d8d2a0631a6e722784bdfa9e787ad86796227de4
FengchenX <fengchenxiaonvsheng@hotmail.com>
2019-04-12 19:25:49 +0800

    add

--- flask/py_note/video/service/svc.py
+++ flask/py_note/video/service/svc.py
@@ -1,9 +1,15 @@
 # from flask import Flask
 from service import base
+from flask import request, json, jsonify
 
 app = base.app
 
 
-@app.route('/video')
+@app.route('/videos')
 def video():
     return "video"
+
+@app.route('/videos', methods=['POST'])
+def addVideo():
+    print(request.get_data())
+    return jsonify({"name":"name", "age":10})
