dc748367a70d1b2fbbebc979825ae965577887a1
FengchenX <fengchenxiaonvsheng@hotmail.com>
2019-04-12 20:34:22 +0800

    add

--- flask/py_note/video/app.py
+++ flask/py_note/video/app.py
@@ -2,12 +2,14 @@ from  service import svc
 from db.pg import sys
 
 if __name__ == '__main__':
-    cur = sys.sysDB.cursor()
-    cur.execute("SELECT * FROM testtb LIMIT 10")
-    rows = cur.fetchall()
-    print(rows)
-    sys.sysDB.commit()
-    cur.close()
-    sys.sysDB.close()
+
+    # cur = sys.sysDB.cursor()
+    # cur.execute("SELECT * FROM testtb LIMIT 10")
+    # rows = cur.fetchall()
+    # print(rows)
+    # sys.sysDB.commit()
+    # cur.close()
+
+    sys.CreateTb()
     
     svc.app.run(host='0.0.0.0')
--- flask/py_note/video/db/pg/sys.py
+++ flask/py_note/video/db/pg/sys.py
@@ -1,3 +1,16 @@
 from db.pg import base
 
 sysDB = base.conn
+
+
+def CreateTb():
+    cur = sysDB.cursor()
+    cur.execute('''CREATE TABLE COMPANY
+       (ID INT PRIMARY KEY     NOT NULL,
+       NAME           TEXT    NOT NULL,
+       AGE            INT     NOT NULL,
+       ADDRESS        CHAR(50),
+       SALARY         REAL);''')
+    
+    sysDB.commit()
+    pass
